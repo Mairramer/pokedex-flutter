4b61c32d112fa014a3a9ee8342bd5f91edca887d
Mairramer <mairramer.dasilva28@hotmail.com>
2021-06-01 19:52:20 -0300

    const de imagens

---
+++ pokedex_flutter/lib/src/core/consts/consts_images.dart
@@ -0,0 +1,5 @@
+class ConstImages {
+  static const String images = 'assets/images/';
+  static const whitePokeball = '${images}pokeball.png';
+  static const blackPokeball = '${images}pokeball_dark.png';
+}
