4dda39c241d3fe709410e703c7216ad937fb6b95
Mairramer <mairramer.dasilva28@hotmail.com>
2021-06-09 21:18:53 -0300

    corrigido alguns erros

--- pokedex_flutter/lib/main.dart
+++ pokedex_flutter/lib/main.dart
@@ -3,10 +3,12 @@ import 'package:get_it/get_it.dart';
 import 'package:pokedex/src/stores/pokeapi_store.dart';
 
 import 'src/pages/home/home_page.dart';
+import 'src/stores/pokeapiv2_store.dart';
 
 void main() {
   GetIt getIt = GetIt.instance;
   getIt.registerSingleton<PokeApiStore>(PokeApiStore());
+  getIt.registerSingleton<PokeApiV2Store>(PokeApiV2Store());
   runApp(MyApp());
 }
 
