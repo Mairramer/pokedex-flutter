45930f5ba4d242deb8bf76925cab6a436086c521
Mairramer <mairramer.dasilva28@hotmail.com>
2021-06-10 22:00:25 -0300

    adicionado traduções

---
+++ pokedex_flutter/lib/src/core/translations/en_US/en_us_translation.dart
@@ -0,0 +1,11 @@
+final Map<String, String> enUs = {
+  'descricao': 'Description',
+  'biologia': 'Biology',
+  'altura': 'height',
+  'peso': 'Weight',
+  'velocidade': 'Speed',
+  'defesa': 'Defense',
+  'ataque': 'Attack',
+  'sobre': 'About',
+  'evolucao': 'Evolution'
+};
---
+++ pokedex_flutter/lib/src/core/translations/pt_BR/pt_br_translation.dart
@@ -0,0 +1,11 @@
+final Map<String, String> ptBR = {
+  'descricao': 'Descrição',
+  'biologia': 'Biologia',
+  'altura': 'Altura',
+  'peso': 'Peso',
+  'velocidade': 'Velocidade',
+  'defesa': 'Defesa',
+  'ataque': 'Ataque',
+  'sobre': 'Sobre',
+  'evolucao': 'Evolução'
+};
